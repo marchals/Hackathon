95e52145e7d0ee3224b86c19b53efcd893f60ee0
marchals <sebastien.marchal@mines-ales.org>
2019-12-04 14:50:49 +0100

    preprocessing des données :
    - suppression colonne : Id, index, from, to, date
    - get_dummies de toutes les colonnes catégoriques
    - séparation de scheduled_time en year, month, day, hour
    - normalization de hour sur 24 heures
    - normalisation de stop_sequence sur le nombre d'arrets max de toute la colonne
    - separation données et labels

---
+++ data_prepro.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 14:40:15 2019
+
+@author: sebastien
+"""
+
+import pandas as pd
+
+# transformer scheduled time en col year, monthn, day, hour normalisé entre 0 et 1
+# à quoi sert col index ?
+    
+
+
+def preprocess(df):
+    
+    # drop useless columns
+    col_to_drop = ["from", "to", "Id", "date", "index"]
+    df.drop(columns=col_to_drop, inplace=True)
+    
+    """====================check NaN values======================="""
+    
+    def check_nan():
+        columns_name = df.columns
+        nan = pd.isna(df)
+        for col in columns_name:
+            print(type(nan[col].value_counts()))
+        
+    """====================split labels======================="""
+    
+    labels = df.pop("delay_minutes")
+    labels = pd.DataFrame(labels)
+    
+    """====================manage schedule_time======================="""
+    
+    def time_second_conv(time):
+        # time must be a datetime object
+        h = time.hour
+        m = time.minute
+        s = time.second
+        return s + m*60 + h*3600
+    
+    # create year, month, day, hour columns
+    df["scheduled_time"] = pd.to_datetime(df["scheduled_time"])
+    df['scheduled_hour'] = [time_second_conv(d.time()) for d in df['scheduled_time']]
+    df['scheduled_day'] = [d.date().day for d in df['scheduled_time']]
+    df['scheduled_month'] = [d.date().month for d in df['scheduled_time']]
+    df['scheduled_year'] = [d.date().year for d in df['scheduled_time']]
+    
+    # drop scheduled_time
+    df.drop(columns=["scheduled_time"], inplace=True)
+    
+    """======================normalization and categorization====================="""
+    
+    df = pd.concat([df.drop("line", axis=1), pd.get_dummies(df["line"])], axis=1)
+    df = pd.concat([df.drop("status", axis=1), pd.get_dummies(df["status"])], axis=1)
+    df = pd.concat([df.drop("from_id", axis=1), pd.get_dummies(df["from_id"])], axis=1)
+    df = pd.concat([df.drop("to_id", axis=1), pd.get_dummies(df["to_id"])], axis=1)
+    df = pd.concat([df.drop("train_id", axis=1), pd.get_dummies(df["train_id"])], axis=1)
+    
+    
+    
+    # normalize hours
+    def normalize_hour(time):
+        #time has to be in second over 24 hours
+        return time/(24*3600)
+    
+    df['scheduled_hour'] = [normalize_hour(i) for i in df['scheduled_hour']]
+    
+    # normalize stop sequence on the global max
+    def normalize_full_column(column):
+        max_val = max(column)
+        for i in range(len(column)):
+            column[i] /= max_val 
+        return column  
+          
+    df["stop_sequence"] = normalize_full_column(df["stop_sequence"])
+
+    return df, labels
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
